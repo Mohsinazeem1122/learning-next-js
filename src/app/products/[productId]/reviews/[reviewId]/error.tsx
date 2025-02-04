5e80878beaf034194e77f379bd07e13c27578fa7
Mohsinazeem1122 <160829626+Mohsinazeem1122@users.noreply.github.com>
2025-02-04 22:34:50 +0500

    handling error

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { startTransition } from "react";
+
+function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={reload}>Try again</button>
+    </div>
+  );
+}
+
+export default ErrorBoundary;
